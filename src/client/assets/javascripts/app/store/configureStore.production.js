22a8c00bc504cc790371f2422f5e703880c2343c
kevind123 <thedude136895@gmail.com>
2018-04-11 15:22:56 +0200

    added redux router middleware to production store.

--- src/client/assets/javascripts/app/store/configureStore.production.js
+++ src/client/assets/javascripts/app/store/configureStore.production.js
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import thunk from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux'
+import { browserHistory } from 'react-router';
 
 import rootReducer from '../reducer';
 
-
+const routerMid = routerMiddleware(browserHistory)
 const middlewares = [
   promiseMiddleware,
-  thunk
+  thunk,
+  routerMid
 ];
 
 const enhancer = compose(
