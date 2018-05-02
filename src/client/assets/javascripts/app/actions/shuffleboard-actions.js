c23f0b2bca92ad2bb8da278156379a348f9d0ed2
kevind123 <thedude136895@gmail.com>
2018-05-02 12:12:09 +0200

    removed unnecessary imports.

--- src/client/assets/javascripts/app/actions/shuffleboard-actions.js
+++ src/client/assets/javascripts/app/actions/shuffleboard-actions.js
@@ -3,7 +3,6 @@ import {
 	Engine,
 	Render,
 	Runner,
-	// Composites,
 	Composite,
 	MouseConstraint,
 	Mouse,
@@ -14,7 +13,6 @@ import {
 } from 'matter-js'
 import {
 	broadcastPucks,
-	// broadcastTurnStarted
 	broadcastMouseDown,
 	broadcastMouseUp,
 	broadcastAcceptModal,
